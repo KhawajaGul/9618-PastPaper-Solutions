1efe719fc63d6f9e17989e8c9b32a474cbee0d7b
KhawajaGul <khwajagm@gmail.com>
2025-04-09 14:20:22 +0500

    ON23 complete 42

--- Yearlies/W23 42/2_N23.py
+++ Yearlies/W23 42/2_N23.py
@@ -12,3 +12,16 @@ def IterativeCalculate(Number):
 
 print(IterativeCalculate(10))
 
+
+
+def RecursiveValue(Number, ToFind):
+    if Number == 0:
+        return 0
+    else:
+        if ToFind % Number == 0:
+            return Number + RecursiveValue((Number - 1), ToFind)
+        else:
+            return RecursiveValue((Number - 1), ToFind)
+        
+
+print(RecursiveValue(50, 50))
